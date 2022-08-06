4c5d68c82d410dfc313dd8ae505d37ca77a9d63e
Mohi Uddin Akbar <makbar@WHM0005366.local>
2022-08-06 12:39:16 +0200

    configuration for eslint is done

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import Hello from "./Hello";
 
-test(`"Hello world should be in the document`, () => {
+it(`"Hello world should be in the document`, () => {
   render(<Hello />);
   const myElement = screen.getByText(/Hello world/i);
+
   expect(myElement).toBeInTheDocument();
 });
